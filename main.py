7c706034b22b1d70d5e55f88f3d3c4e465178beb
stepan vinokurov <vinstp@yandex.ru>
2024-06-02 00:41:37 +0300

    add dockerfile

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ app = Client(
     bot_token=config.BOT_TOKEN
 )
 
+async def send_message(old_message):
+    usernames_string = ''
+    async for member in app.get_chat_members(old_message.chat.id):
+        if member.user.username != 'all_tag_tg_bot':
+            usernames_string += '@' + member.user.username + ' '
+    print(usernames_string)
+
+    await app.send_message(chat_id=old_message.chat.id, text=usernames_string)
+    print(old_message.text)
+
+
 @app.on_message(filters.text)
 async def echo(client, message):
-    if '@all' in message.text:
-        if len(message.text) > 4:
-            if message.text[4] != ' ':
-                return
+    words = message.text.split()
 
-        usernames_string = ''
-        async for member in app.get_chat_members(message.chat.id):
-            if member.user.username != 'all_tag_tg_bot':
-                usernames_string += '@' + member.user.username + ' '
-        print(usernames_string)
-
-        await app.send_message(chat_id=message.chat.id, text=usernames_string)
-        print(message.text)
+    for word in words:
+        if word == '@all':
+            await send_message(message)
+            return
 
 app.run()
 
